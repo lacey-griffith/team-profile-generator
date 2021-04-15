86c2ca5e6090e37355f322e7eea60a5089ba860a
Lacey <lacey.griffith@g.austincc.edu>
2021-04-15 13:40:16 -0500

    turn employee function into class

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,4 +1,4 @@
-// const { TestScheduler, TestWatcher } = require('@jest/core');
+const { TestScheduler } = require('@jest/core');
 // const { test, expect, jest } = require('@jest/globals');
 // const { expect } = require('@jest/globals');
 
@@ -8,25 +8,27 @@ jest.mock('../lib/Employee');
 console.log(new Employee());;
 
 test('creating employee object', () => {
-    const employee = new Employee('Lacey');
+    const employee = new Employee('Lacey')
 
-    expect(employee.name).toEqual(expect.stringContaining(employee.name.toString()));
-    expect(employee.role).toEqual(expect.stringContaining(employee.role.toString()));
+    expect(employee.name).toEqual(expect.stringContaining(employee.name.toString()))
+    expect(employee.role).toEqual(expect.stringContaining(employee.role.toString()))
     expect(employee.id).toEqual(expect.any(Number));
-});
+})
 
 test('get employee name', () => {
-    const employee = new Employee('Lacey');
+    const employee = new Employee('Lacey')
 
-    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
-});
+    expect(employee.getName()).toBe('Lacey')
+})
 
 test('get employee role', () => {
     const employee = new Employee('Lacey')
-    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role.toString()));
-});
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role.toString()))
+})
 
 test('get employee id', () => {
-    const employee = new Employee('Lacey');
-    expect(employee.getID()).toEqual(expect.any(Number));
-});
+    const employee = new Employee('Lacey')
+
+    expect(employee.getID()).toEqual(expect.any(Number))
+})
