eba03a2ac04c3fcf2b7151824fe1bcb3c09c4592
Lacey <lacey.griffith@g.austincc.edu>
2021-04-18 21:27:00 -0500

    prompts user cwith questions, additional questions for specific roles

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
-const pageTemplate = require('./src/page-template.js');
-const {writeFile, copyFile} = require('./utils/generate-site.js');
+const fs = require('fs');
+
+const Employee = require('./lib/Employee')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+
 
 const newEmployeeArr = [];
+
+const init = () => {
+    addEmployee();
+}
 
 const addEmployee = () => {
     console.log('=== === ===')
     console.log('Team Profile Builder')
     console.log('=== === ===')
-    return inquirer.prompt([
-        {
+    return inquirer.prompt([{
             name: 'name',
             type: 'input',
             message: 'Name: ',
@@ -52,129 +59,132 @@ const addEmployee = () => {
             message: 'Which type of employee would you like to add?',
             choices: ['Intern', 'Manager', 'Engineer']
         }])
-};
-
-// const checkRole = employeeDataObj => {
-//         const {name, id, email, role} = employeeDataObj
-        
-//         if(role === 'Intern') {
-//             return addIntern(employeeDataObj)
-//         }
-//         if(role === 'Manager') {
-//             return addManager(employeeDataObj)
-//         }
-//         if(role === 'Engineer') {
-//             return  addEngineer(employeeDataObj)
-//         }
-// }
-
-const addIntern = employeeDataObj => {
-    inquirer.prompt({
-        name: 'school',
-        type: 'input',
-        message: `Which school did ${employeeDataObj.name} attend?`
-    }).then(school => {
-        employeeDataObj.school = school
-        
-        inquirer.prompt({
-            name: 'confirmAddEmployee',
-            type: 'confirm',
-            message: 'Would you like to add another employee?',
-        })
         .then(employeeDataObj => {
-            if(employeeDataObj.confirmAddEmployee) {
-                return addEmployee(employeeDataObj);
-            } else {
-                newEmployeeArr.push(employeeDataObj)
-                //console.log(newEmployeeArr)
-                return employeeDataObj
+            const {
+                name,
+                id,
+                email,
+                role
+            } = employeeDataObj
+
+            if (role === 'Intern') {
+            return inquirer.prompt({
+                    name: 'school',
+                    type: 'input',
+                    message: `Which school did ${employeeDataObj.name} attend?`
+                }).then(school => {
+                    const employee = new Intern(name, role, id, email, school)
+                    inquirer.prompt({
+                        name: 'confirmAddEmployee',
+                        type: 'confirm',
+                        message: 'Would you like to add another employee?'
+                    }).then(employeeDataObj => {
+                        if (employeeDataObj.confirmAddEmployee) {
+                            return addEmployee();
+                        } else {
+                            newEmployeeArr.push(employeeDataObj)
+                            return employeeDataObj
+                        }
+
+                    })
+                })
+            }
+            if (role === 'Manager') {
+            return inquirer.prompt({
+                    name: 'officeNumber',
+                    type: 'input',
+                    message: `What is ${employeeDataObj.name}'s office phone number?`
+                }).then(officeNumber => {
+                    const employee = new Manager(name, role, id, email, officeNumber)
+                    inquirer.prompt({
+                        name: 'confirmAddEmployee',
+                        type: 'confirm',
+                        message: 'Would you like to add another employee?'
+                    }).then(employeeDataObj => {
+                        if (employeeDataObj.confirmAddEmployee) {
+                            return addEmployee();
+                        } else {
+                            newEmployeeArr.push(employeeDataObj)
+                            return employeeDataObj
+                        }
+
+                    })
+                })
+            }
+            if (role === 'Engineer') {
+            return inquirer.prompt({
+                    name: 'github',
+                    type: 'input',
+                    message: `What is ${employeeDataObj.name}'s GitHub username?`
+                }).then(github => {
+                    const employee = new Engineer(name, role, id, email, github)
+                    inquirer.prompt({
+                        name: 'confirmAddEmployee',
+                        type: 'confirm',
+                        message: 'Would you like to add another employee?'
+                    }).then(employeeDataObj => {
+                        if (employeeDataObj.confirmAddEmployee) {
+                            return addEmployee();
+                        } else {
+                            newEmployeeArr.push(employeeDataObj)
+                            return employeeDataObj
+                        }
+                    })
+                })
             }
         })
-    })
 }
 
-const addManager = employeeDataObj => {
-    inquirer.prompt({
-        name: 'officeNumber',
-        type: 'input',
-        message: `What is ${employeeDataObj.name}'s office phone number?`
-    }).then(officeNumber => {
-        employeeDataObj.officeNumber = officeNumber
-        
-        inquirer.prompt({
-            name: 'confirmAddEmployee',
-            type: 'confirm',
-            message: 'Would you like to add another employee?',
-        })
-        .then(employeeDataObj => {
-            if(employeeDataObj.confirmAddEmployee) {
-                return addEmployee(employeeDataObj);
-            } else {
-                newEmployeeArr.push(employeeDataObj)
-                //console.log(newEmployeeArr)
-                return employeeDataObj
-            }
-        })
-    })
-}
+const generateHTML = function(){
+    console.log(newEmployeeArr)
+    return `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Our Team Profile</title>
+        </head>
+        <body>
+            TESTING THIS RIGHT NOW
+        </body>
+    </html>
+        `;
+    }
 
-const addEngineer = employeeDataObj => {
-    inquirer.prompt({
-        name: 'github',
-        type: 'input',
-        message: `What is ${employeeDataObj.name}'s GitHub username?`
-    }).then(github => {
-        employeeDataObj.github = github
-        
-        inquirer.prompt({
-            name: 'confirmAddEmployee',
-            type: 'confirm',
-            message: 'Would you like to add another employee?',
-        })
-        .then(employeeDataObj => {
-            if(employeeDataObj.confirmAddEmployee) {
-                console.log(employeeDataObj, 'line 136')
-                return addEmployee(employeeDataObj);
-            } else {
-                // console.log(newEmployeeArr)
-                return employeeDataObj
-            }
-        })
-    })
-}
 
-addEmployee()
-.then(employeeDataObj => {
-    const {name, id, email, role} = employeeDataObj
-    if(role === 'Intern') {
-       return addIntern(employeeDataObj)
-    }
-    if(role === 'Manager') {
-        //if manager is selected, user is returned 
-       return addManager(employeeDataObj)
-    }
-    if(role === 'Engineer') {
-        //if engineer is selected, github prompts then employeedataobj is undefined before option to answer
-       return addEngineer(employeeDataObj)
-    }
-})
-.then(newEmployeeArr => {
-    //below console log gives undefined 
-    console.log(employeeDataObj,'line 160')
-    //console.log(newEmployeeArr, 'line 162 index')
-    // newEmployeeArr is undefined here
-    return pageTemplate(newEmployeeArr)
-})
-// .then(profileHTML => {
-//     return writeFile(profileHTML)
-// })
-// .then(writeFileResponse => {
-//     console.log(writeFileResponse)
-//     return copyFile();
-// })
-.then(copyFileResponse => {
-    console.log(copyFileResponse)
-})
-.catch(err => {
-    console.log(err)
-})
+
+
+
+// const writeFile = profileHTML => {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile('./dist/index.html', profileHTML, err => {
+//             if (err) {
+//                 reject(err);
+//                 return
+//             }
+//             resolve({
+//                 ok: true,
+//                 message: 'File created successfully.'
+//             })
+//         })
+//     })
+// };
+
+// const copyFile = () => {
+//     return new Promise ((resolve, reject) => {
+//         fs.copyFile('./src/style.css','./dist/style.css', err => {
+//             if (err) {
+//                 reject(err)
+//                 return
+//             }
+//             resolve({
+//                 ok: true,
+//                 message: 'File copied successfully.'
+//             })
+//         }
+//     )}
+// )};
+
+init();
